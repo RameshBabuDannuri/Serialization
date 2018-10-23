e5a89dff0a2c1af335663dd31940f4b9dfc314aa
Ramesh Babu <ramesh.dannuri@gmail.com>
2018-10-23 14:18:22 +0530

    Update OrderOfSerializationDemo1.java

--- src/Serialization/OrderOfSerializationDemo1.java
+++ src/Serialization/OrderOfSerializationDemo1.java
@@ -130,7 +130,9 @@ public class OrderOfSerializationDemo1 {
 }
 /*
 * Program 2: De-serialization is done in different than serialization order
-In this program, irrespective of whether we know serialization order or NOT, we will perform de-serialization in some random order
+------------------------------------------------------------------------------------
+In this program, irrespective of whether we know serialization order or NOT, we will perform de-serialization 
+in some random order
 
 
 
@@ -150,88 +152,8 @@ Employee
 
 OrderOfSerializationDeSerialization.java
 
-?
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
+
+
 package in.bench.resources.serialization;
 
 import java.io.FileInputStream;
@@ -315,14 +237,6 @@ public class OrderOfSerializationDeSerialization {
 }
 Output:
 
-?
-1
-2
-3
-4
-5
-6
-7
 Serialization: All objects saved to OrderOfObjects.ser file
 
 Exception in thread "main" java.lang.ClassCastException: in.bench.resources
@@ -336,13 +250,16 @@ Explanation:
 
 Here serialization order is Customer –> Employee –> Student
 But we are de-serializing in different order i.e.; Student –> Customer –> Employee
-So, while de-serializing 1st time when we read object from serialized file, it returns Customer object, as we serialized Customer object first
-But instead of type-casting to Customer object, we type-casted to Student object –> which results in throwing java.lang.ClassCastException
+So, while de-serializing 1st time when we read object from serialized file, it returns Customer object, as we
+serialized Customer object first
+But instead of type-casting to Customer object, we type-casted to Student object –> which results in throwing 
+java.lang.ClassCastException
 To overcome this exception, we can use instanceOf operator
 Move to program 3 –> for much improved version using instanceOf operator
 
 
  Program 3: When order of serialization is unknown, how can we overcome this situation?
+ =========================================================================================================
 We can use instanceOf operator to check the respective object first iterating through while loop
 
 Later, we can assign it to correct class by type-casting
@@ -353,102 +270,7 @@ But we can catch this exception and take corrective action (like here, we can pr
 
 OrderOfSerializationDeSerialization.java
 
-?
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
+
 package in.bench.resources.serialization;
 
 import java.io.EOFException;
@@ -544,20 +366,7 @@ public class OrderOfSerializationDeSerialization {
         System.out.println(deSerializeStudent);
     }
 }
-Output:
 
-?
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
 Serialization: All objects saved to OrderOfObjects.ser file
 
 De-Serialization: All objects de-serialized from OrderOfObjects.ser file
